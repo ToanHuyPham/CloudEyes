a1ae4f791917160a1351e2bd0340d5fdb8b396f5
Toan Huy Pham <toanhuy.nt0@gmail.com>
2026-08-06 12:17:07 +0700

    Update __init__.py

--- core/cloudeyes_core/cohorts/__init__.py
+++ core/cloudeyes_core/cohorts/__init__.py
@@ -3,10 +3,14 @@
 from .builder import build_cohorts
 from .compatibility import CompatibilityResult, compare_samples
 from .key import build_cohort_key
+from .summary import CohortSummary, MetricSummary, summarize_cohort
 
 __all__ = [
+    "CohortSummary",
     "CompatibilityResult",
+    "MetricSummary",
     "build_cohort_key",
     "build_cohorts",
     "compare_samples",
+    "summarize_cohort",
 ]
