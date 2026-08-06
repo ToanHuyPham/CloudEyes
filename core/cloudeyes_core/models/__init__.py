9bb9621c3f6629f7da5e5cd6164fbf25351e3f65
Toan Huy Pham <toanhuy.nt0@gmail.com>
2026-08-06 12:23:24 +0700

    Update __init__.py

--- core/cloudeyes_core/models/__init__.py
+++ core/cloudeyes_core/models/__init__.py
@@ -1,6 +1,8 @@
 """Public CloudEyes core data models."""
 
 from .cohort import Cohort, CohortKey
+from .confidence import Confidence, ConfidenceLevel
+from .coverage import Coverage
 from .identity import MachineIdentity, ProductIdentity, ProviderIdentity
 from .measurement import Measurement, MeasurementStatus
 from .metric import Metric, MetricDirection
@@ -10,6 +12,9 @@ from .sample import Sample, SampleQuality, SampleQualityStatus
 __all__ = [
     "Cohort",
     "CohortKey",
+    "Confidence",
+    "ConfidenceLevel",
+    "Coverage",
     "MachineIdentity",
     "Measurement",
     "MeasurementStatus",
