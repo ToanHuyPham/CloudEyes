65d790143c8e61d4047efc3eade1a8b2c98331cc
Toan Huy Pham <toanhuy.nt0@gmail.com>
2026-08-06 12:16:16 +0700

    Update __init__.py

--- core/cloudeyes_core/statistics/__init__.py
+++ core/cloudeyes_core/statistics/__init__.py
@@ -1,4 +1,12 @@
-"""Statistics.
+"""Statistical utilities used by CloudEyes."""
+
+from .descriptive import SummaryStatistics, percentile, summarize
+
+__all__ = [
+    "SummaryStatistics",
+    "percentile",
+    "summarize",
+]"""Statistics.
 
 Placeholder for the CloudEyes foundation.
 Implementation will be added with tests and documentation.
